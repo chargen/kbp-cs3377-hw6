1db96bd0bd582515fbf0856bbcbd5047162d5a9d
quietsamurai98 <quietsamurai98@users.noreply.github.com>
2018-04-23 19:44:35 -0500

    Added binary file header reading and display

--- main.cc
+++ main.cc
@@ -1,4 +1,4 @@
-/*
+ /*
  * Source code for Program 6
  *
  * Based on the example code from Dr. Perkins
@@ -11,19 +11,47 @@
  */
 
 #include <iostream>
+#include <cstdint>
+#include <fstream>
 #include "cdk.h"
 
 
 #define MATRIX_WIDTH 3
 #define MATRIX_HEIGHT 5
-#define BOX_WIDTH 15
+#define BOX_WIDTH 30
 #define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
 
+class BinaryFileHeader{
+public:
+  uint32_t magicNumber;
+  uint32_t versionNumber;
+  uint64_t numRecords;
+};
+
+const int maxRecordStringLength = 25;
+class BinaryFileRecord{
+public:
+  uint8_t strLength;
+  char stringBuffer[maxRecordStringLength];
+};
 
 int main()
 {
+  ifstream binary_reader("cs3377.bin",ios::in|ios::binary);
+  BinaryFileHeader *bin_head = new BinaryFileHeader();
+  binary_reader.read((char *)bin_head, sizeof(BinaryFileHeader));
+  cout << bin_head->magicNumber << endl;
+  cout << bin_head->versionNumber << endl;
+  cout << bin_head->numRecords << endl;
+  
+  char magic[maxRecordStringLength];
+  sprintf(magic, "Magic: 0x%X",bin_head->magicNumber);
+  char version[maxRecordStringLength];
+  sprintf(version, "Version: %u",bin_head->versionNumber);
+  char records[maxRecordStringLength];
+  sprintf(records, "NumRecords: %lu",bin_head->numRecords);
 
   WINDOW	*window;
   CDKSCREEN	*cdkscreen;
@@ -31,7 +59,7 @@ int main()
 
   const char 		*rowTitles[MATRIX_HEIGHT+1] = {"", "a", "b", "c", "d", "e"};
   const char 		*columnTitles[MATRIX_WIDTH+1] = {"", "a", "b", "c"};
-  int		boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
+  int		boxWidths[MATRIX_WIDTH+1] = {maxRecordStringLength,maxRecordStringLength,maxRecordStringLength,maxRecordStringLength};
   int		boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
 
   /*
@@ -64,7 +92,9 @@ int main()
   /*
    * Dipslay a message
    */
-  setCDKMatrixCell(myMatrix, 1, 1, "Magic");
+  setCDKMatrixCell(myMatrix, 1, 1, magic);
+  setCDKMatrixCell(myMatrix, 1, 2, version);
+  setCDKMatrixCell(myMatrix, 1, 3, records);
   drawCDKMatrix(myMatrix, true);    /* required  */
 
   /* so we can see results */
